b1873e063a7e2f140f06c83040139c65b64ec1e2
dweavp <100860878+pashatett@users.noreply.github.com>
2025-01-10 10:09:42 +0300

    Update laba3.cpp

--- laba3/laba3.cpp
+++ laba3/laba3.cpp
@@ -9,186 +9,231 @@
 using namespace std;
 
 void Task1();
-void Task1_1(int n);
-
 void Task2();
-void Task2_1(int a, int b);
-
 void Task3();
-void Task3_1(int secretNumber);
 
+int gcd(int m, int n);
 void Task4();
-int Task4_1(int a, int b);
 
+
+
+void Task5_1(double x, double eps);
 void Task5();
-float Task5_1(int n, float x, float eps);
 
 void Task6();
-void Task6_1(float x, float xk, float xd, float a, float b, float c);
-float Task6_2(float x, float a, float b, float c);
+void Task6_1(double x_start, double x_end, double dx, double a, double b, double c);
 
 void Task7();
 double Task7_1(int n, float x);
 
 void Task8();
-int Task8_1(int n);
-int Task8_2(int n);
+int fibonacci(int n);
+int fibonacci_non_recursive(int n);
 
 void Task9();
-int M(int n);
+int mccarthy_cycle(int n);
+int mccarthy_recursion(int n);
 
 
 
 int main()
 {
 	setlocale(LC_ALL, "");
+	srand(unsigned(time(0)));
+	cout << "*******************************" << endl;
+	cout << setw(45) << "Лабораторная работа №3" << endl;
+	cout << setw(40) << "Одномерные массивы" << endl;
+	cout << "Выполнил студент группы 151412" << endl;
+	cout << setw(49) << "Пирогов Павел Алексеевич" << endl;
+	cout << setw(30) << "Вариант №17" << endl;
+	cout << "*******************************" << endl << endl;
 	int n;
-	cout<<"Какую задачу?";cin>>n;
+	cout << "Какую задачу хотите запустить? "; cin >> n;
 	switch (n) {
-case 1:
-	Task1();
-	break;
-case 2:
-	Task2();
-	break;
-case 3:
-	Task3();
-	break;
-case 4:
-	Task4();
-	break;
-case 5:
-	Task5();
-	break;
-case 6:
-	Task6();
-	break;
-case 7:
-	Task7();
-	break;
-case 8:
-	Task8();
-	break;
-case 9:
-	Task9();
-	break;
-}
-	Task5();
+	case 1:
+		Task1();
+		break;
+	case 2:
+		Task2();
+		break;
+	case 3:
+		Task3();
+		break;
+	case 4:
+		Task4();
+		break;
+	case 5:
+		Task5();
+		break;
+	case 6:
+		Task6();
+		break;
+	case 7:
+		Task7();
+		break;
+	case 8:
+		Task8();
+		break;
+	case 9:
+		Task9();
+		break;
+
+	default:
+		cout << "Такой задачи нет((";
+		break;
+	}
 }
 
 void Task1() {
 	int n;
-	cout << "Введите число n "; cin >> n;
-	Task1_1(n);
-}
-void Task1_1(int n){
 	double P = 0.0;
+
+	// Ввод n с клавиатуры
+	cout << "Введите n: ";
+	cin >> n;
+
+	// Вычисление суммы
 	for (int i = 1; i <= n; i++) {
 		P += 1.0 / (i * (i + 1));
 	}
-	cout << "P = " << P;
+
+	// Вывод результата
+	cout << "Значение P: " << P << endl;
 }
 
 void Task2() {
-	int a,b;
+	int* a = new int[2];
+	int n;
 	do {
-		cout << "Введите первое число "; cin >> a;
-		cout << "Введите второе число отличное от первого "; cin >> a;
-	} while (max(a,b) - min(a,b) == 0);
+		cout << "Введите первое число "; cin >> a[0];
+		cout << "Введите второе число "; cin >> a[1];
+		sort(a, a + 2);
+	} while (a[1] - a[0] == 0);
 
-	Task2_1(a,b);
-}
-void Task2_1(int a, int b){
-    int n;
-    do {
+	do {
 		cout << "Введите  число "; cin >> n;
-	} while (n<min(a,b) || n>max(a,b));
-	cout << "Ура Вы угадали(надо было вписать число из диапазона)";
+	} while (n<a[0] || n>a[1]);
+	cout << "Ура Вы угадали";
 }
 
 void Task3() {
-	srand(time(0));
 	int secretNumber = rand() % 100 + 1;
-
-	Task3_1(secretNumber);
-}
-void Task3_1(int secretNumber){
-    int userGuess = 0, countt = 0;
-    while (userGuess != secretNumber) {
+	int userGuess = 0;
+	int count = 0;
+	while (userGuess != secretNumber) {
 		cout << "Введите ваше число: "; cin >> userGuess;
-		countt++;
-		if (userGuess > secretNumber)
+		count++;
+		if (userGuess > secretNumber) {
 			cout << "Перелет! Попробуйте ввести число меньше." << endl;
-		else if (userGuess < secretNumber)
+		}
+		else if (userGuess < secretNumber) {
 			cout << "Недолет! Попробуйте ввести число больше." << endl;
+		}
 	}
-	cout << "Поздравляю! Вы угадали число " << secretNumber << " за " << countt << " попыток!" << endl;
+	cout << "Поздравляю! Вы угадали число " << secretNumber << " за " << count << " попыток!" << endl;
+}
+
+
+int gcd(int m, int n) {
+	if (m == n) {
+		return m;
+	}
+
+	while (m != n) {
+		if (m > n) {
+			m -= n;
+		}
+		else {
+			n -= m;
+		}
+	}
+
+	return m;
 }
 
 void Task4() {
-	int a, b;
-	cout << "Введите два числа" << endl;
+	int num1, num2;
+	cout << "Введите два целых числа: ";
+	cin >> num1 >> num2;
 
-    cout << "Введите первое число "; cin >> a;
-    cout << "Введите второе число "; cin >> b;
+	int result = gcd(num1, num2);
+	cout << "НОД чисел " << num1 << " и " << num2 << " равен " << result << std::endl;
 
-	cout<<Task4_1(a,b);
 }
-int Task4_1(int a, int b){
-    while (a != b) {
-		if (a > b)
-			a -= b;
-		else
-			b -= a;
+
+
+void Task5_1(double x, double eps) {
+	double y = 1;
+	double y_prev = 1;
+
+	int n = 1;
+
+	while (abs(y - y_prev) >= eps) {
+		y_prev = y;
+		y = 0.5 * (y_prev + x / y_prev);
+		n++;
 	}
-	return a;
+
+	cout << "y = " << y << endl;
+	cout << "n = " << n << endl;
 }
 
 void Task5() {
-	float eps, x;
-	int n;
-	cout << "Введите число x "; cin >> x;
-	cout << "Введите число eps "; cin >> eps;
-	cout << "Введите число "; cin >> n;
-	float x1 = Task5_1(n, x, eps);
-	float y = 1, a = 0;
-	while (abs(y - a) >= eps) {
-		a = y;
-		y = (a + x1 / a) / 2;
-	}
-	cout << "y = " << y;
-}
-float Task5_1(int n, float x, float eps) {
-	if (n == 1) return 1;
-	else return 0.5 * (Task5_1(n - 1, x, eps) + x / Task5_1(n - 1, x, eps));
+	double x, eps;
+
+	cout << "Enter x: ";
+	cin >> x;
+
+	cout << "Enter eps: ";
+	cin >> eps;
+	Task5_1(x,eps);
 }
 
-void Task6() {
-	float xn, xk, xd;
-	cout << "Введите Xнач Хкон dX: "; cin >> xn >> xk >> xd;
-	float a, b, c;
-	cout << "Введите a b c: "; cin >> a >> b >> c;
-	Task6_1(xn, xk, xd, a, b, c);
-}
-void Task6_1(float xn, float xk, float xd, float a, float b, float c) {
-	int a1 = int(a), b1 = int(b), c1 = int(c);
-	float x = xn;
-	int ee = (a1 | b1) & ((a1 | c1));
-	while (x <= xk) {
-		if (ee)
-			cout << setw(10)<<x << setw(10) << Task6_2(x, a, b, c) << endl;
-		else
-			cout << setw(10) << x << setw(10) << int(Task6_2(x, a, b, c)) << endl;
-		x += xd;
+void Task6_1(double x_start, double x_end, double dx, double a, double b, double c) {
+	cout << "x | F(x)" << endl;
+	cout << "--------------" << endl;
+	for (double x = x_start; x <= x_end; x += dx) {
+		double F;
+		if (x + 10 < 0 && b != 0) {
+			F = (a * x * x - c * x + b) / (x - a);
+		}
+		else if (x + 10 > 0 && b == 0) {
+			F = (x - c) / (x - a);
+		}
+		else {
+			F = -x / (a - c);
+		}
+
+		int ac = floor(a);
+		int bc = floor(b);
+		int cc = floor(c);
+		bool condition = ((ac || bc) && !(ac || cc));
+
+		cout << x << " | ";
+		if (condition) {
+			cout << static_cast<int>(F);
+		}
+		else {
+			cout << F;
+		}
+		cout << endl;
 	}
 }
-float Task6_2(float x, float a, float b, float c) {
-	if (x + 10 < 0 && b != 0)
-		return a * x * x - c * x + b;
-	else if (x + 10 > 0 && b == 0)
-		return (x - a) / (x - c);
-	else
-		return -x / (a - c);
+void Task6() {
+	double a, b, c, x_start, x_end, dx;
+	cout << "Введите значение a: ";
+	cin >> a;
+	cout << "Введите значение b: ";
+	cin >> b;
+	cout << "Введите значение c: ";
+	cin >> c;
+	cout << "Введите начальное значение x (Xнач.): ";
+	cin >> x_start;
+	cout << "Введите конечное значение x (Xкон.): ";
+	cin >> x_end;
+	cout << "Введите шаг (dX): ";
+	cin >> dx;
+	Task6_1(x_start, x_end,  dx,  a,  b,  c);
 }
 
 void Task7() {
@@ -197,47 +242,63 @@ void Task7() {
 	do {
 		cout << "Введите х (0<x<=2) n: "; cin >> x >> n;
 	} while (x <= 0 || x > 2);
-	double lnx = Task7_1(n,x);
+	double lnx = Task7_1(n, x);
 	cout << "Натуральный логарифм будет равен " << lnx;
 }
 double Task7_1(int n, float x) {
-	double y = 0;
+	double y = 0.0;
 	for (int i = 0; i <= n; i++) {
 		y += pow(-1, n) * pow(x - 1, n + 1) / (n + 1);
 	}
 	return y;
 }
 
-void Task8() {
-	int a;
-	cout << "Введите число "; cin >> a;
-	cout << Task8_1(a);
-}
-int Task8_1(int n) {
-	if (n == 0)return 0;
-	else if (n == 1)return 1;
-	else return Task8_1(n - 1) + Task8_2(n - 2);
-}
-int Task8_2(int n) {
-	int* a = new int[n+1];
-	a[0] = 0;
-	a[1] = 1;
-	for(int i=2;i<=n;i++){
-        a[i] = a[i-1] + a[i-2];
+int fibonacci(int n) {
+	if (n == 0) {
+		return 0;
 	}
-	return a[n];
+	else if (n == 1) {
+		return 1;
+	}
+	else {
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+}
+
+int fibonacci_non_recursive(int n) {
+	return fibonacci(n);
+}
+void Task8() {
+	int n;
+	cout << "Введите n: ";
+	cin >> n;
+	cout << "числа фибоначи: " << fibonacci_non_recursive(n) << endl;
+}
+
+int mccarthy_cycle(int n) {
+	while (n >= 100) {
+		n -= 10;
+	}
+	while (n < 100) {
+		n += 11;
+	}
+	return n;
+}
+
+int mccarthy_recursion(int n) {
+	if (n >= 100) {
+		return n - 10;
+	}
+	else {
+		return mccarthy_recursion(n + 11);
+	}
 }
 
 void Task9() {
 	int n;
-	cout << "Введите число "; cin >> n;
-	cout << M(n) << endl << endl;
+	cout << "Введите целое число n: ";
+	cin >> n;
 
-
-	while (n < 100) n += 11;
-	cout << n - 10;
-}
-int M(int n) {
-	if (n >= 100) return n - 10;
-	else return M(n + 11);
+	cout << "Значение функции Маккарти (цикл): " << mccarthy_cycle(n) << endl;
+	cout << "Значение функции Маккарти (рекурсия): " << mccarthy_recursion(n) << endl;
 }
